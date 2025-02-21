23cb7f0b399e946a6035bd790ce62e4dcada433b
Bhushan Rokade <83108525+http-bhushan@users.noreply.github.com>
2025-02-21 21:10:11 +0530

    changed to youtube scrapper

--- backend/core_programs/youtube-transcript.ts
+++ backend/core_programs/youtube-transcript.ts
@@ -1,12 +1,32 @@
-const { YoutubeTranscript } = require('youtube-transcript');
+import { getSubtitles } from 'youtube-captions-scraper';
+
 type TransObj = {
   text: string;
   duration: number;
   offset: number;
   lang: string;
 };
-function getTranscript(url: string): Array<TransObj> {
-  const transcript: Array<TransObj> = YoutubeTranscript.fetchTranscript(url);
-  return transcript;
+
+async function getTranscript(
+  videoId: string,
+  lang: string = 'en'
+): Promise<Array<TransObj>> {
+  try {
+    const transcript = await getSubtitles({
+      videoID: videoId, // YouTube Video ID
+      lang: lang, // Language (default: English)
+    });
+
+    return transcript.map((item) => ({
+      text: item.text,
+      duration: item.dur,
+      offset: item.start,
+      lang: lang,
+    }));
+  } catch (error) {
+    console.error('Error fetching transcript:', error);
+    return [];
+  }
 }
+
 export default getTranscript;
